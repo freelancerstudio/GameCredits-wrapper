1231149fdddba8669c8de26c1bdb032e017a64de
gamers-coin <asksecinfo@gmail.com>
2014-04-30 00:17:12 +0200

    PUSH

---
+++ ClientTest/Controllers/HomeController.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Web.Mvc;
+using GamerscoinWrapper.Data;
+using GamerscoinWrapper.Wrapper;
+using GamerscoinWrapper.Wrapper.Interfaces;
+
+namespace WebClient.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly IBaseBtcConnector _baseBtcConnector;
+        private readonly IGamerscoinService _GamerscoinService;
+
+        //
+        // GET: /Home/
+        public HomeController()
+        {
+            _baseBtcConnector =  new BaseBtcConnector();
+            _GamerscoinService = new GamerscoinService();
+        }
+
+        public ActionResult Index()
+        {
+            return View("Index");
+        }
+
+        [HttpGet]
+        public Decimal GetBalanceInWallet()
+        {
+            return _baseBtcConnector.GetBalance();
+        }
+
+        [HttpGet]
+        public JsonResult GetInformationAboutTransaction(String txId)
+        {
+            Transaction transaction = _GamerscoinService.GetTransaction(txId);
+            return Json(transaction, JsonRequestBehavior.AllowGet);
+        }
+
+        [HttpGet]
+        public JsonResult GetBlockInfo(String blockhashId)
+        {
+            Block transaction = _baseBtcConnector.GetBlock(blockhashId);
+            return Json(transaction, JsonRequestBehavior.AllowGet);
+        }
+    }
+}
---
+++ WrapperClient/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Configuration;
+using System.Globalization;
+using GamerscoinWrapper.Wrapper;
+using GamerscoinWrapper.Wrapper.Interfaces;
+
+namespace ConsoleClient
+{
+    internal sealed class Program
+    {
+        static void Main()
+        {
+            IBaseBtcConnector baseBtcConnector = new BaseBtcConnector(true); // Use Primary Wallet
+            Console.Write("Connecting to Gamerscoin daemon: " + ConfigurationManager.AppSettings["ServerIp"] + "...");
+            Double networkDifficulty = baseBtcConnector.GetDifficulty();
+            Console.WriteLine("OK\n\nBTC Network Difficulty: " + networkDifficulty.ToString("#,#", CultureInfo.InvariantCulture));
+            Decimal myBalance = baseBtcConnector.GetBalance();
+            Console.WriteLine("My balance: " + myBalance + " BTC");
+            Console.ReadLine();
+        }
+    }
+}
