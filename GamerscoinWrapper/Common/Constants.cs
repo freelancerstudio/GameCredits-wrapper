4254270abe707a4e71e8fbc4f8fc4fc97c11f2fd
gamers-coin <asksecinfo@gmail.com>
2014-05-30 18:04:58 +0200

    Small Update for  GetBalance

--- GamerscoinWrapper/Common/Constants.cs
+++ GamerscoinWrapper/Common/Constants.cs
@@ -4,6 +4,6 @@ namespace GamerscoinWrapper.Common
 {
     public class Constants
     {
-        public const String GenesisBlock = "0x91ec5f25ee9a0ffa1af7d4da4db9a552228dd2dc77cdb15b738be4e1f55f30ee";
+        public const String GenesisBlock = "0x000000000019d6689c085ae165831e934ff763ae46a2a6c172b3f1b60a8ce26f";
     }
 }
